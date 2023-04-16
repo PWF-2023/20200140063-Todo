9ebf03c2150eaa5f3f7ee87daf8cfaf75e9aa8ca
prgtyfzh <pragitya.faazha.ft20@mail.umy.ac.id>
2023-04-17 00:20:49 +0700

    update, delete data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -27,4 +27,31 @@ class UserController extends Controller
         // dd($users->toArray());
         return view('user.index', compact('users'));
     }
+    public function makeadmin(User $user)
+    {
+        $user->timestamps = false;
+        $user->is_admin = true;
+        $user->save();
+        return back()->with('success', 'Make admin successfully!');
+    }
+    public function removeadmin(User $user)
+    {
+        if ($user->id != 1) {
+            $user->timestamps = false;
+            $user->is_admin = false;
+            $user->save();
+            return back()->with('success', 'Remove admin successfullly!');
+        } else {
+            return redirect()->route('user.index');
+        }
+    }
+    public function destroy(User $user)
+    {
+        if ($user->id != 1) {
+            $user->delete();
+            return back()->with('success', 'Delete user successfully!');
+        } else {
+            return redirect()->route('user.index')->with('danger', 'Delete user failed!');
+        }
+    }
 }
